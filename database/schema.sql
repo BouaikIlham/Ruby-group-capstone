2b17ea3c5cae30a5dedc99462a3bfd92aa80eb7e
principles31 <principles31@gmail.com>
2022-08-17 09:53:47 +0000

    Add item table

--- database/schema.sql
+++ database/schema.sql
@@ -2,3 +2,16 @@
 CREATE DATABASE catalog;
 
 --Create tables based on diagram
+
+
+CREATE TABLE Items(
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
+  genre_id     INT NOT NULL,
+  author_id    INT NOT NULL,
+  label_id     INT NOT NULL,
+  publish_date DATE NOT NULL,
+  archived     BOOLEAN NOT NULL,
+  CONSTRAINT label_fk FOREIGN KEY (label_id) REFERENCES Labels (id),
+  CONSTRAINT author_fk FOREIGN KEY (author_id) REFERENCES Authors (id),
+  CONSTRAINT genre_fk FOREIGN KEY (genre_id) REFERENCES Genres (id)
+);
