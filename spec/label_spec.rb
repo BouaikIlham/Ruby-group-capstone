e685595c404543f95f7d124fc53642eb8d65946b
principles31 <principles31@gmail.com>
2022-08-18 10:55:02 +0000

    Fix error

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -1,4 +1,5 @@
 require_relative 'spec_helper'
+
 describe Label do
   before(:each) do
     @old_book = Book.new('04-09-2009', 'Amazon', 'good')
@@ -6,16 +7,20 @@ describe Label do
     @bad_book = Book.new('04-09-2022', 'Google', 'bad')
     @label = Label.new('Science fiction', 'White')
   end
+
   context 'testing the Label class' do
     it 'Should an instance of Label class' do
       expect(@label).to be_an_instance_of Label
     end
+
     it 'Check title of the label ' do
       expect(@label.title).to eq('Science fiction')
     end
+
     it 'Check title of the label' do
       expect(@label.color).to eq('White')
     end
+
     it 'Check labels items' do
       @label.add_item(@old_book)
       @label.add_item(@new_book)
