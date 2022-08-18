5b8b634305acaed705975042f0c18d44b229d33b
ilham bouaik <ibouaik@gmail.com>
2022-08-18 12:02:56 +0100

    fix testing errors

--- spec/album_rspec.rb
+++ spec/album_rspec.rb
@@ -1,4 +1,4 @@
-require_relative 'spec_helper'
+require_relative 'rspec_helper'
 
 describe MusicAlbum do
   context 'MusicAlbum class' do
--- spec/author_rspec.rb
+++ spec/author_rspec.rb
@@ -1,4 +1,4 @@
-require_relative 'spec_helper'
+require_relative 'rspec_helper'
 
 describe Author do
   before(:each) do
